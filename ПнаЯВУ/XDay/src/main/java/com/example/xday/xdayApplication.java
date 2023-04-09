ae019288fa9bbe9a734c6fcabbe28337aaa422b1
therealonemcbanana <89997340+therealonemcbanana@users.noreply.github.com>
2023-04-10 00:56:30 +0300

    Add files via upload

---
+++ ПнаЯВУ/XDay/src/main/java/com/example/xday/xdayApplication.java
@@ -0,0 +1,13 @@
+package com.example.xday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class xdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(xdayApplication.class, args);
+	}
+
+}
